5e0c3038e76503e875557761a2c3be78efda0cae
Erick Godoy <erickgodoy725@gmail.com>
2022-10-28 16:56:23 -0500

    title color palevioletred

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 function TodoItem(props){
     return (
-        <li>
+        <li className='TodoItem'>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>ck</span>
+
             <p>Title</p>
-            <p>Description</p>
             <span>{props.text}</span>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--completed'}`}>fail</span>
+            
         </li>
     )
 }

2b6a334de68f64450ee20ea3f6d6e7e97387d464
Erick Godoy <erickgodoy725@gmail.com>
2022-10-30 21:25:21 -0500

    appUI

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 import './App.css';
 import React from 'react';
-import {TodoCounter} from './TodoCounter'
-import {TodoSearch} from './TodoSearch'
-import {TodoList} from './TodoList'
-import {TodoItem} from './TodoItem'
-import {CreateTodoButton} from './CreateTodoButton'
+import {AppUI} from './AppUI'
 
 const defaultTodos = [
   { text: "Limpiar escritorio", completed: false },
@@ -47,28 +43,15 @@ function App() {
 
 
   return (
-    <React.Fragment>
-      <TodoCounter
-      total = {totalTodos}
-      completed = {completedTodos}
-      />
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-       <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text} 
-          completed={todo.completed}
-          onComplete = {() => completeTodo(todo.text)}
-          onDelete = {() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-      <CreateTodoButton />
-    </React.Fragment>
+   <AppUI 
+   totalTodos = {totalTodos}
+   completedTodos = {completedTodos}
+   searchValue={searchValue}
+   setSearchValue={setSearchValue}
+   searchedTodos = {searchedTodos}
+   completeTodo = {completeTodo}
+   deleteTodo = {deleteTodo}
+   />
   );
 }
 
